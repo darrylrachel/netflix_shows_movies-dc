d66565c55668d2ae4a6de05aabf9499e5b50a8c2
Darryl Rachel <racheldarryl@icloud.com>
2024-06-04 16:37:36 -0700

    Standardized data

--- Netflix-DC.sql
+++ Netflix-DC.sql
@@ -4,7 +4,6 @@ FROM credits;
 SELECT *
 FROM titles;
 
-
 Select c.id, c.name, c.role, t.title, t.release_year, t.type, t.genres
 From credits c
 Join titles t 
@@ -45,16 +44,31 @@ From credits;
 Select *
 From titles;
 
-Select c.id, c.name, c.role, t.title, t.release_year, t.type, t.genres
+Select *
+From titles
+Where age_certification = '';
+
+Update titles
+Set age_certification = 'Not Rated'
+Where age_certification = '';
+
+Select c.person_id, c.id, t.type, t.title, c.name, c.role, c.character
 From credits c
 Join titles t 
 	On c.id = t.id
-Order By name ASC;
+Order By c.id ASC;
 
+Select `character`
+From credits
+Where `character` = '';
 
+Update credits
+Set `character` = Null
+Where `character` = '';
 
-
-
+Update credits
+Set `character` = 'DIRECTOR'
+Where role = 'DIRECTOR';
 
 
 
